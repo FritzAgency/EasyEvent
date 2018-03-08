cef443fa8a0a4bfc42bc2e1bd41128924dddf985
charmming <charmming4u@gmail.com>
2018-03-08 14:03:28 +0100

    adding the modal to the menu

--- resources/views/Public/index.blade.php
+++ resources/views/Public/index.blade.php
@@ -46,19 +46,19 @@
 		@else
 		@endif	
 							
-                                <li><a href="#">Create Event</a></li>
-                                <li><a href="#">Start selling</a></li>
+                                <li><a href="#" data-toggle="modal" data-target="#myModal">Create Event</a></li>
+                                <li><a href="#" data-toggle="modal" data-target="#myModal">Start selling</a></li>
 								<li><a href="#">Products/Services</a></li>
 								
 								@if (!Auth::guest())
 									@else
-                                <li><a href="/login">Login</a></li>
+                                <li><a href="/login" data-toggle="modal" data-target="#myModal">Login</a></li>
 							@endif
 							
 							@if(!Auth::guest())
         
                                 @else
-								<li> <a href="/signup"> Sign up </a> </li> 	
+								<li> <a href="/signup" data-toggle="modal" data-target="#myModal"> Sign up </a> </li> 	
 								@endif
 								
 								<!-- logout button only shown to the logged in users --> 
@@ -70,6 +70,42 @@
 									
                             </ul>
                             </div>
+                            <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header" style="padding:35px 50px;">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+        </div>
+        <div class="modal-body" style="padding:40px 50px;">
+          <form role="form">
+            <div class="form-group">
+              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+              <input type="text" class="form-control" id="usrname" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <input type="text" class="form-control" id="psw" placeholder="Enter password">
+            </div>
+            <div class="checkbox">
+              <label><input type="checkbox" value="" checked>Remember me</label>
+            </div>
+              <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+          <p>Not a member? <a href="#">Sign Up</a></p>
+          <p>Forgot <a href="#">Password?</a></p>
+        </div>
+      </div>
+      
+    </div>
+  </div> 
+</div>
                         </nav> 
             </header>
  	
