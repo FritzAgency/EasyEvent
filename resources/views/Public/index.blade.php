b18239f0bee1b82a0ee3b8db719e373dbade88d2
Adetona <adetonaabiodun12@gmail.com>
2018-03-08 10:13:16 +0100

    Homepage structure

--- resources/views/Public/index.blade.php
+++ resources/views/Public/index.blade.php
@@ -5,28 +5,54 @@
 <head>
 
 <title> Easy Events </title> 
+<link  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+
 
 </head> 
 
 <body>
 
-<h1> Homepage1 </h1> 
+<div class="container-fluid">  
 
-<a href="#CreateEvent"> Create Event </a>  </br> 
+<div class="row"> 
+<div class="col-md-2">
+Easy Events. 
+</div> 
 
-<a href="/signup" > Signup </a> </br> 
+<div class="col-md-offset-2 col-md-2"> 
+@if(!Auth::guest())
+<a href="#">{{ Auth::user()->first_name }}</a> 
+@else
+@endif 
+</div> 
 
+<div class="col-md-2">
+<a href="/signup"> Create Event </a>  </br> 
+</div> 
+
+<div class="col-md-2">
+<a href="#"> Start Selling </a> 
+
+</div> 
+
+
+
+<div class="col-md-2">
 @if (!Auth::guest())  
 <a href="{{ route('showSelectOrganiser') }}">Dashboard</a>
 @else
 <h1> <a href="{{ route('login')}}"> Login </a> </h1>
 @endif
+</div> 
 
+<div class="col-md-2">
 @if(!Auth::guest())
 <a href="{{ route ('logout')}}"> 
 @else
 @endif	
-	
+</div> 	
+</div> 
+</div> 	
 </body> 
 
 
